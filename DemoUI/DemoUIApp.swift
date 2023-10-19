ff67aa0dc1df7aa0d9e3f0e4d3ab0c14989c29c2
Lokesh Kumawat <lokeshkumawat27@gmail.com>
2023-10-19 22:35:47 +0530

    Initial Commit

---
+++ DemoUI/DemoUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoUIApp.swift
+//  DemoUI
+//
+//  Created by MR Tailor on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
